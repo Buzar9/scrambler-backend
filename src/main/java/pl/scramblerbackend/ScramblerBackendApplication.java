b8989e6060e558157e05c2548ca0be7807e552c8
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-05-23 12:00:07 +0200

    Initial Commit

---
+++ src/main/java/pl/scramblerbackend/ScramblerBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.scramblerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScramblerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScramblerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/scramblerbackend/ScramblerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.scramblerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScramblerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
