b08a4ac2e7acf72b75e86de17ed9aa843825b872
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-05-24 09:13:28 +0200

    SylabicController completed

--- src/main/java/pl/scramblerbackend/controller/SylabicController.java
+++ src/main/java/pl/scramblerbackend/controller/SylabicController.java
@@ -1,4 +1,21 @@
 package pl.scramblerbackend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.scramblerbackend.service.SylabicService;
+
+@RestController
+@RequestMapping("/sylabic")
+@CrossOrigin(origins = "*")
 public class SylabicController {
+
+    @Autowired
+    private SylabicService sylabicService;
+
+    @PostMapping
+    public String encrypt(@RequestBody String key,
+                          @RequestBody String inMessage) {
+
+        return sylabicService.encrypt(key, inMessage);
+    }
 }
