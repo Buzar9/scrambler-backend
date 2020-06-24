8c985b0624f599ad3d8499198260c81d1e1e89ac
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-06-24 20:08:48 +0200

    Create SylabicDao.java

---
+++ src/main/java/pl/scramblerbackend/dao/SylabicDao.java
@@ -0,0 +1,26 @@
+package pl.scramblerbackend.dao;
+
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@Component
+public class SylabicDao {
+
+    public List<Character> vowelReader() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("/Users/marek/Documents/GitHub/scrambler-backend/src/main/resources/static/Vowel.txt"));
+        List<Character> listedVowel = new ArrayList<>();
+        int counter = 0;
+
+        while (scanner.hasNextLine()) {
+            listedVowel.add(counter, scanner.nextLine().charAt(0));
+            counter++;
+        }
+
+        return listedVowel;
+    }
+}
