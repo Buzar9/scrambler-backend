267e522a6f5888fdc3a0bac244afe999ba1052ff
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-06-24 20:08:09 +0200

    Create OutPassword.java
    Entity for communication with fronted

---
+++ src/main/java/pl/scramblerbackend/entity/OutPassword.java
@@ -0,0 +1,18 @@
+package pl.scramblerbackend.entity;
+
+public class OutPassword {
+
+    private String password;
+
+    public OutPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
