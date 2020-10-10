194591c4a7c51ba8f7271cf30ef92312a4002061
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-10-10 22:49:35 +0200

    Code review after method chapter

--- src/main/java/pl/scramblerbackend/service/MorsService.java
+++ src/main/java/pl/scramblerbackend/service/MorsService.java
@@ -18,93 +18,163 @@ public class MorsService {
     @Autowired
     private MorsDao morsDao;
 
-    public OutPassword encrypt(KeyNMessage morsMessage) throws FileNotFoundException {
+    public OutPassword encrypt(KeyNMessage keyNMessage) throws FileNotFoundException {
 
+        String roughMessage = keyNMessage.getMessage().toLowerCase();
         Map<Integer, Character> mappedAlphabet = morsDao.latinReader();
         Map<Integer, String> mappedMors = morsDao.morsReader();
-        List<String> listedPassword = new ArrayList<>();
-        String result = new String();
+        String password;
 
-//        Choosing method of encryption.
-        String roughMessage = morsMessage.getMessage().toLowerCase();
 
-        int firstLetter = roughMessage.charAt(0);
-        if (firstLetter >= 65) {
-
-//           Standardization of messages to the supported format.
-            Map<Integer, Character> mappedLetter = new HashMap<>();
-            String standardizedMessage = roughMessage.toLowerCase();
-            for (int j = 0; j < standardizedMessage.length(); j++) {
-                mappedLetter.put(j, standardizedMessage.charAt(j));
-            }
-
-//            Encryption latin to mors.
-            for (int t = 0; t < mappedLetter.size(); t++) {
-                for (int r = 0; r < mappedAlphabet.size(); r++) {
-                    if (mappedLetter.get(t) == mappedAlphabet.get(r)) {
-                        listedPassword.add(mappedMors.get(r));
-                        break;
-                    }
-                    if (mappedLetter.get(t) == 32){
-                        listedPassword.add("");
-                        break;
-                    }
-                }
-            }
-
-//            Prepare result format.
-            result = "///";
-            for (String letter : listedPassword) {
-                result += letter + "/";
-            }
-            result += "//";
-
-//            Validation.
-            for (int y = 0; y < mappedLetter.size(); y++) {
-                if (!mappedAlphabet.containsValue(mappedLetter.get(y)) && mappedLetter.get(y) != 32) {
-                    result = "Niepoprawny znak";
-                }
-            }
+        if (isMessageInLatin(roughMessage)) {
+            Map<Integer, Character> lettersFromMessage = mappingLettersFromMessage(roughMessage);
+            password = encryptionLatinToMors(lettersFromMessage, mappedMors, mappedAlphabet);
 
         } else {
+            Map<Integer, String> mappedSign = mappingSignFromMessage(roughMessage);
+            password = encryptionMorsToLatin(mappedSign, mappedMors, mappedAlphabet);
 
-//            Standardization of messages to the supported format.
-            String substringMessage = roughMessage
-                    .substring(3, roughMessage.length()-3)
-                    .replace("//","/|/");
-            String[] splittedMessage = substringMessage.split("/");
-            Map<Integer, String> mappedSign = new HashMap<>();
+        }
 
-            for (int q = 0; q < splittedMessage.length; q++) {
-                String trimMessage = splittedMessage[q].trim();
-                mappedSign.put(q, trimMessage);
-            }
+        return new OutPassword(password);
+    }
 
-//            Encryption mors to latin.
-            for (int o = 0; o < mappedSign.size(); o++) {
-                for (int p = 0; p < mappedMors.size(); p++) {
-                    if (mappedSign.get(o).equals(mappedMors.get(p)) & !mappedSign.get(o).equals("|")) {
-                        listedPassword.add(mappedAlphabet.get(p).toString());
-                        break;
-                    } else if (mappedSign.get(o).equals("|")) {
-                        listedPassword.add(" ");
-                    }
-                }
-            }
+    private boolean isMessageInLatin(String roughMessage) {
+        int firstLetter = roughMessage.charAt(0);
+        boolean isLatin = firstLetter >= 65;
+        return isLatin;
+    }
 
-//            Prepare result format.
-            for (String letter : listedPassword) {
-                result += letter;
-            }
+    private Map<Integer, Character> mappingLettersFromMessage(String roughMessage) {
+        Map<Integer, Character> mappedLetters = new HashMap<>();
+        for (int i = 0; i < roughMessage.length(); i++) {
+            mappedLetters.put(i, roughMessage.charAt(i));
+        }
+        return mappedLetters;
+    }
 
-//            Validation.
-            for (int i = 0; i < mappedSign.size(); i++) {
-                if (!mappedMors.containsValue(mappedSign.get(i)) & !mappedSign.get(i).equals("|")) {
-                    result = "Niepoprawny znak";
+    private String encryptionLatinToMors(Map<Integer, Character> lettersFromMessage,
+                                         Map<Integer, String> mappedMors,
+                                         Map<Integer, Character> mappedAlphabet) {
+        List<String> roughPassword = latinEncryptionToRoughMorsPassword(lettersFromMessage, mappedMors, mappedAlphabet);
+        String readyPassword = adaptPasswordToConvention(roughPassword);
+
+        if (morsPasswordValidation(lettersFromMessage, mappedAlphabet)) {
+            return readyPassword;
+        }
+        return "Niepoprawny znak";
+    }
+
+    private List<String> latinEncryptionToRoughMorsPassword(Map<Integer, Character> lettersFromMessage,
+                                                        Map<Integer, String> mappedMors,
+                                                        Map<Integer, Character> mappedAlphabet) {
+        List<String> roughPassword = new ArrayList<>();
+
+        for (int t = 0; t < lettersFromMessage.size(); t++) {
+            for (int r = 0; r < mappedAlphabet.size(); r++) {
+                if (lettersFromMessage.get(t) == mappedAlphabet.get(r)) {
+                    roughPassword.add(mappedMors.get(r));
+                    break;
+                } else if (lettersFromMessage.get(t) == 32) {
+                    roughPassword.add("");
+                    break;
                 }
             }
         }
+        return roughPassword;
+    }
 
-        return new OutPassword(result);
+    private String adaptPasswordToConvention(List<String> roughPassword) {
+        String readyPassword = "///";
+        for (String letter : roughPassword) {
+            readyPassword += letter + "/";
+        }
+        readyPassword += "//";
+        return readyPassword;
+    }
+
+
+    private boolean morsPasswordValidation(Map<Integer, Character> lettersFromMessage,
+                                           Map<Integer, Character> mappedAlphabet) {
+        for (int y = 0; y < lettersFromMessage.size(); y++) {
+            if (!mappedAlphabet.containsValue(lettersFromMessage.get(y)) && lettersFromMessage.get(y) != 32) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Map<Integer, String> mappingSignFromMessage(String roughMessage) {
+        String substringMessage = erasingUnnecessaryContentFromMessage(roughMessage);
+
+        String[] splitMessage = substringMessage.split("/");
+        Map<Integer, String> mappedSign = new HashMap<>();
+
+        for (int q = 0; q < splitMessage.length; q++) {
+            String trimMessage = splitMessage[q].trim();
+            mappedSign.put(q, trimMessage);
+        }
+        return mappedSign;
+    }
+
+    private String erasingUnnecessaryContentFromMessage(String roughMessage) {
+        String substringMessage = roughMessage
+                .substring(3, roughMessage.length() - 3)
+                .replace("//", "/|/");
+        return substringMessage;
+    }
+
+    private String encryptionMorsToLatin(Map<Integer, String> mappedSign,
+                                         Map<Integer, String> mappedMors,
+                                         Map<Integer, Character> mappedAlphabet) {
+
+        List<String> roughPassword = morsEncryptionToRoughLatinPassword(mappedSign, mappedMors, mappedAlphabet);
+        String readyPassword = createSentenceFromLetters(roughPassword);
+
+        if (latinPasswordValidation(mappedSign, mappedMors)) {
+            return readyPassword;
+        }
+
+        return "Niepoprawny znak";
+
+    }
+
+    private List<String> morsEncryptionToRoughLatinPassword(Map<Integer, String> mappedSign,
+                                                            Map<Integer, String> mappedMors,
+                                                            Map<Integer, Character> mappedAlphabet) {
+
+        List<String> roughPassword = new ArrayList<>();
+
+        for (int o = 0; o < mappedSign.size(); o++) {
+            for (int p = 0; p < mappedMors.size(); p++) {
+                if (mappedSign.get(o).equals(mappedMors.get(p)) & !mappedSign.get(o).equals("|")) {
+                    roughPassword.add(mappedAlphabet.get(p).toString());
+                    break;
+                } else if (mappedSign.get(o).equals("|")) {
+                    roughPassword.add(" ");
+                }
+            }
+        }
+        return roughPassword;
+    }
+
+    private String createSentenceFromLetters(List<String> roughPassword) {
+        String readyPassword = new String();
+        for (String letter : roughPassword) {
+            readyPassword += letter;
+        }
+
+        return readyPassword;
+    }
+
+    private boolean latinPasswordValidation(Map<Integer, String> mappedSign,
+                                           Map<Integer, String> mappedMors) {
+        for (int i = 0; i < mappedSign.size(); i++) {
+            if (!mappedMors.containsValue(mappedSign.get(i)) & !mappedSign.get(i).equals("|")) {
+                return false;
+            }
+        }
+        return true;
+
     }
 }
